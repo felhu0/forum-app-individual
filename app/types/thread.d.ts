8802bf74283d519e214de3be6c1ff4f25f8b8c00
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-08-29 13:09:35 +0200

    Add functionality to get all and create threads and add comments

--- app/types/thread.d.ts
+++ app/types/thread.d.ts
@@ -1,12 +1,21 @@
+import { Timestamp } from "firebase/firestore";
 import { User } from "./user";
 
 type ThreadCategory = "NEW" | "HOT";
 
-type Thread = {
-  id: number;
+export type Comment = {
+  id: string;
+  content: string;
+  creationDate: Timestamp;
+  creator: User;
+}
+
+export type Thread = {
+  id: string;
   title: string;
   category: ThreadCategory;
-  creationDate: Date;
+  creationDate: Timestamp;
   description: string;
   creator: User;
+  comments: Comment[];
 }
