1369843cb23a9af724ca2d2d2b2a140fa7f62106
Felicia Hu <feliciahu1992_@hotmail.com>
2024-08-29 13:33:28 +0200

    chore: Update package-lock.json and authProvider.tsx

---
+++ app/_components/authProvider.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { auth } from "@/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+  User,
+  UserCredential,
+} from "firebase/auth";
+
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import toast from "react-hot-toast";
+
+type AuthValues = {
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+};
+type AuthContextType = {
+  user: User | null;
+  authLoaded: boolean;
+  register: (values: AuthValues) => Promise<string | void>;
+  login: (values: AuthValues) => Promise<void>;
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [authLoaded, setAuthLoaded] = useState<boolean>(false);
+
+  // Check if there is a logged in user
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (_user) => {
+      setUser(_user);
+      setAuthLoaded(true);
+    });
+
+    return () => unsub();
+  }, []);
+
+  const register = async (values: AuthValues): Promise<string | void> => {
+    const toastId = toast.loading("Creating account...");
+
+    try {
+      const userCredential: UserCredential =
+        await createUserWithEmailAndPassword(
+          auth,
+          values.email,
+          values.password
+        );
+
+      if (!userCredential.user) {
+        throw new Error("Something went wrong!. Please try again.");
+      }
+      console.log(userCredential);
+
+      await updateProfile(userCredential.user, {
+        displayName: `${values.firstName} ${values.lastName}`,
+      });
+
+      setUser(userCredential.user);
+
+      toast.success("Account created successfully", { id: toastId });
+
+      return userCredential.user.uid;
+    } catch (error: any) {
+      console.log(error.message);
+      console.log(error.code);
+      const message = error.code.split("/")[1].replace(/-/g, " ");
+      toast.error(message || error.message, { id: toastId });
+    }
+  };
+
+  const login = async (values: AuthValues): Promise<void> => {
+    const toastId = toast.loading("Signing in...");
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
+
+      if (!userCredential.user) {
+        throw new Error("Something went wrong!. Please try again.");
+      }
+      console.log(userCredential);
+      const token = await userCredential.user.getIdToken();
+      console.log("token:", token);
+
+      toast.success("Logged in successfully", { id: toastId });
+    } catch (error: any) {
+      console.log(error.message);
+      const message = error.code.split("/")[1].replace(/-/g, " ");
+      toast.error(message || error.message, { id: toastId });
+    }
+  };
+
+  const value = {
+    user,
+    authLoaded,
+    register,
+    login,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context)
+    throw new Error("useAuth must be used within an AuthContextProvider");
+  return context;
+};
