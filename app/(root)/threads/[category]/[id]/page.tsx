f0c0b89825b800568706f9e198e410619deead96
Emma Mellgren <mellgrenemma0@gmail.com>
2024-09-02 12:17:57 +0200

    Comment box styling

--- app/(root)/threads/[category]/[id]/page.tsx
+++ app/(root)/threads/[category]/[id]/page.tsx
@@ -1,231 +1,220 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
-import { getThreadById, lockThread, updateThread } from "@/lib/thread.db";
-import { FaQuestionCircle, FaUnlock, FaLock } from "react-icons/fa";
+import { useEffect, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { getThreadById, lockThread, updateThread } from '@/lib/thread.db';
+import { FaUnlock, FaLock } from 'react-icons/fa';
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
-import { Comments } from "@/app/_components/Comments";
-import { NewCommentForm } from "@/app/_components/NewCommentForm";
-import { Thread, Comment } from "@/app/types/thread";
-import { User } from "@/app/types/user";
-import { useAuth } from "@/app/_components/authProvider";
-import { Badge } from "@/components/ui/badge";
-import Loading from "@/app/_components/Loading";
+import { Comments } from '@/app/_components/Comments';
+import { NewCommentForm } from '@/app/_components/NewCommentForm';
+import { Thread, Comment } from '@/app/types/thread';
+import { User } from '@/app/types/user';
+import { useAuth } from '@/app/_components/authProvider';
+import { Badge } from '@/components/ui/badge';
+import Loading from '@/app/_components/Loading';
 
 type Params = {
-  id: string;
+    id: string;
 };
 
 const ThreadDetailsPage = () => {
-  const [thread, setThread] = useState<Thread | null>(null);
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [answered, setAnswered] = useState<boolean>(false);
-  const [answeredCommentId, setAnsweredCommentId] = useState<string | null>(
-    null
-  );
-  const [threadCreatorId, setThreadCreatorId] = useState<User | null>(null);
-  const { user } = useAuth();
-  const router = useRouter();
-  const { id } = useParams<Params>();
-  const [loading, setLoading] = useState(true);
+    const [thread, setThread] = useState<Thread | null>(null);
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [answered, setAnswered] = useState<boolean>(false);
+    const [answeredCommentId, setAnsweredCommentId] = useState<string | null>(
+        null
+    );
+    const [threadCreatorId, setThreadCreatorId] = useState<User | null>(null);
+    const { user } = useAuth();
+    const router = useRouter();
+    const { id } = useParams<Params>();
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchThread = async () => {
-      if (id) {
-        try {
-          const fetchedThread = await getThreadById(id);
-          if (fetchedThread) {
-            setThread(fetchedThread);
-            setComments(fetchedThread.comments);
-            setThreadCreatorId(fetchedThread.creator);
-            setAnswered(fetchedThread.isAnswered ?? false);
-            setAnsweredCommentId(fetchedThread.answeredCommentId ?? null);
-          } else {
-            console.log("No thread found with the given ID.");
-            router.push("/404");
-          }
-        } catch (error) {
-          console.error("Error fetching thread data:", error);
-        } finally {
-          setLoading(false);
+    useEffect(() => {
+        const fetchThread = async () => {
+            if (id) {
+                try {
+                    const fetchedThread = await getThreadById(id);
+                    if (fetchedThread) {
+                        setThread(fetchedThread);
+                        setComments(fetchedThread.comments);
+                        setThreadCreatorId(fetchedThread.creator);
+                        setAnswered(fetchedThread.isAnswered ?? false);
+                        setAnsweredCommentId(
+                            fetchedThread.answeredCommentId ?? null
+                        );
+                    } else {
+                        console.log('No thread found with the given ID.');
+                        router.push('/404');
+                    }
+                } catch (error) {
+                    console.error('Error fetching thread data:', error);
+                } finally {
+                    setLoading(false);
+                }
+            } else {
+                console.log('ID is not available in search parameters.');
+            }
+        };
+
+        fetchThread();
+    }, [id, router]);
+
+    const handleCommentSubmit = async (newComment: Comment) => {
+        if (thread) {
+            setComments([...comments, newComment]);
         }
-      } else {
-        console.log("ID is not available in search parameters.");
-      }
     };
 
-    fetchThread();
-  }, [id, router]);
+    const handleMarkAsAnswered = async (commentId: string) => {
+        try {
+            if (!thread) {
+                console.error('Thread not found.');
+                return;
+            }
 
-  if (loading) {
-    return <Loading />;
-  }
+            const newIsAnswered = answeredCommentId !== commentId;
 
-  const handleCommentSubmit = async (newComment: Comment) => {
-    if (thread) {
-      setComments([...comments, newComment]);
-    }
-  };
+            const fieldsToUpdate: Partial<Thread> = {
+                isAnswered: newIsAnswered,
+            };
 
-  const handleMarkAsAnswered = async (commentId: string) => {
-    try {
-      if (!thread) {
-        console.error("Thread not found.");
-        return;
-      }
+            if (newIsAnswered) {
+                fieldsToUpdate.answeredCommentId = commentId;
+            } else {
+                fieldsToUpdate.answeredCommentId = null;
+            }
 
-      const newIsAnswered = answeredCommentId !== commentId;
+            await updateThread(thread.id, fieldsToUpdate);
 
-      const fieldsToUpdate: Partial<Thread> = {
-        isAnswered: newIsAnswered,
-      };
+            setAnswered(newIsAnswered);
+            setAnsweredCommentId(newIsAnswered ? commentId : null);
+        } catch (error) {
+            console.error('Error toggling comment as answered:', error);
+        }
+    };
 
-      if (newIsAnswered) {
-        fieldsToUpdate.answeredCommentId = commentId;
-      } else {
-        fieldsToUpdate.answeredCommentId = null;
-      }
+    const handleToggleLock = async () => {
+        if (!thread) return;
 
-      await updateThread(thread.id, fieldsToUpdate);
+        try {
+            await lockThread(thread.id, !thread.isLocked);
+            setThread({ ...thread, isLocked: !thread.isLocked });
+        } catch (error) {
+            console.error('Failed to lock/unlock thread.', error);
+        }
+    };
 
-      setAnswered(newIsAnswered);
-      setAnsweredCommentId(newIsAnswered ? commentId : null);
-    } catch (error) {
-      console.error("Error toggling comment as answered:", error);
-    }
-  };
+    if (loading || !thread) return <Loading />;
 
-  const handleToggleLock = async () => {
-    if (!thread) return;
-
-    try {
-      await lockThread(thread.id, !thread.isLocked);
-      setThread({ ...thread, isLocked: !thread.isLocked });
-    } catch (error) {
-      console.error("Failed to lock/unlock thread.", error);
-    }
-  };
-
-  if (!thread) {
     return (
-      <div className="flex pt-16 text-center justify-center mx-auto text-lg font-medium">
-        Loading...
-      </div>
-    );
-  }
+        <main className='flex flex-col justify-between min-h-screen'>
+            <div className='w-full mx-auto pl-12 px-6 max-w-6xl my-8 pt-6'>
+                <div className='rounded-md border'>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className='text-stone-600 text-sm font-semibold p-4 bg-stone-50 flex items-center justify-between h-auto w-full'>
+                                    <div>
+                                        <p>{thread.title}</p>
+                                    </div>
+                                    <div
+                                        className='
+                                    mr-2'>
+                                        {user && (
+                                            <button onClick={handleToggleLock}>
+                                                {thread.isLocked ? (
+                                                    <Badge
+                                                        variant='destructive'
+                                                        className='rounded-full p-3'>
+                                                        <FaLock className='h-3 w-3' />
+                                                    </Badge>
+                                                ) : (
+                                                    <Badge
+                                                        variant='secondary'
+                                                        className='rounded-full p-3 transition-all duration-300 hover:scale-105'>
+                                                        <FaUnlock className='h-3 w-3' />
+                                                    </Badge>
+                                                )}
+                                            </button>
+                                        )}
+                                    </div>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <div className='py-3 pl-1 pr-6 text-base'>
+                                        {thread.description}
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                        <TableBody>
+                            <TableRow className='bg-stone-50'>
+                                <TableCell>
+                                    <div className='flex justify-between items-center'>
+                                        <span className='flex gap-1 text-muted-foreground'>
+                                            By
+                                            <p className='font-bold text-indigo-700'>
+                                                {' '}
+                                                {thread.creator.name}
+                                            </p>
+                                        </span>
 
-  return (
-    <>
-      <div className="w-full mx-auto pl-12 px-6 max-w-6xl my-8 pt-6">
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="text-stone-600 text-sm font-semibold p-4 bg-stone-50 flex items-center justify-between h-auto w-full">
-                  <div>
-                    <p>{thread.title}</p>
-                  </div>
-                  <div
-                    className="
-                                    mr-2"
-                  >
-                    {user && (
-                      <button onClick={handleToggleLock}>
-                        {thread.isLocked ? (
-                          <Badge
-                            variant="destructive"
-                            className="rounded-full p-3"
-                          >
-                            <FaLock className="h-3 w-3" />
-                          </Badge>
-                        ) : (
-                          <Badge
-                            variant="secondary"
-                            className="rounded-full p-3 transition-all duration-300 hover:scale-105"
-                          >
-                            <FaUnlock className="h-3 w-3" />
-                          </Badge>
-                        )}
-                      </button>
+                                        {thread.isQnA && (
+                                            <Badge variant='qna'>Q&A</Badge>
+                                        )}
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className='rounded-md border mt-6'>
+                    {thread && (
+                        <Comments
+                            comments={comments}
+                            threadId={thread.id}
+                            threadCreatorId={thread.creator.id}
+                            answered={answered}
+                            setAnswered={setAnswered}
+                            handleAnswered={handleMarkAsAnswered}
+                            answeredCommentId={answeredCommentId ?? null}
+                            isQnA={thread.isQnA ?? false}
+                            isLocked={thread.isLocked}
+                        />
                     )}
-                  </div>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <div className="py-3 pl-1 pr-6 text-base">
-                    {thread.description}
-                  </div>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-            <TableBody>
-              <TableRow className="bg-stone-50">
-                <TableCell>
-                  <div className="flex justify-between items-center">
-                    <span className="flex gap-1 text-muted-foreground">
-                      By
-                      <p className="font-bold text-indigo-700">
-                        {" "}
-                        {thread.creator.name}
-                      </p>
-                    </span>
+                </div>
+            </div>
+            {!thread.isLocked && (
+                <div className='w-full pl-12 px-6 py-8 relative bottom-0 bg-gray-200'>
+                    <div className='mx-auto max-w-3xl'>
+                        <NewCommentForm
+                            id={thread.id}
+                            onCommentSubmit={handleCommentSubmit}
+                        />
+                    </div>
+                </div>
+            )}
 
-                    {thread.isQnA && <Badge variant="qna">Q&A</Badge>}
-                  </div>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </div>
-      </div>
-      <div className="mx-auto w-full pl-12 px-6 my-8 max-w-6xl">
-        <div className="rounded-md border">
-          {thread && (
-            <Comments
-              comments={comments}
-              threadId={thread.id}
-              threadCreatorId={thread.creator.id}
-              answered={answered}
-              setAnswered={setAnswered}
-              handleAnswered={handleMarkAsAnswered}
-              answeredCommentId={answeredCommentId ?? null}
-              isQnA={thread.isQnA ?? false}
-              isLocked={thread.isLocked}
-            />
-          )}
-        </div>
-      </div>
-      {!thread.isLocked && (
-        <div className="w-full pl-12 px-6 py-8 relative bottom-0 bg-slate-200">
-          <div className="mx-auto max-w-3xl">
-            <NewCommentForm
-              id={thread.id}
-              onCommentSubmit={handleCommentSubmit}
-            />
-          </div>
-        </div>
-      )}
-
-      {thread.isLocked && (
-        <div className="w-full pl-12 px-6 py-8 bg-red-100 text-red-500 text-center">
-          This thread is locked. No further comments can be added.
-        </div>
-      )}
-    </>
-  );
+            {thread.isLocked && (
+                <div className='w-full pl-12 px-6 py-8 bg-red-100 text-red-500 text-center'>
+                    This thread is locked. No further comments can be added.
+                </div>
+            )}
+        </main>
+    );
 };
 
 export default ThreadDetailsPage;
